51cb6ca4d8f5d1680b0e2b0910ed1f0626193f1f
Priyanshu Vishwakarma <144537220+Priyaanshhu@users.noreply.github.com>
2025-02-17 11:07:22 +0530

    Update

---
+++ Sets/set.py
@@ -0,0 +1,19 @@
+# Set
+
+# set is an unordered collection of unique elements. It's a mutable data structure, meaning it can be modified after creation. 
+
+# Create a set
+my_set = {1, 2, 3, 4, 5}
+print("Original Set:", my_set)
+
+# Add an element to the set
+my_set.add(6)
+print("Set after adding 6:", my_set)
+
+# Remove an element from the set
+my_set.remove(4)
+print("Set after removing 4:", my_set)
+
+# Clear the set
+my_set.clear()
+print("Set after clearing:", my_set)
