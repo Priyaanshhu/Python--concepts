7871741fcffdc1a635d37a7308bb0e2b8a7487fe
Priyanshu Vishwakarma <144537220+Priyaanshhu@users.noreply.github.com>
2025-02-22 15:10:56 +0530

    Update

---
+++ lambda expression/lambda expression.py
@@ -0,0 +1,7 @@
+# lambda expression
+
+# lambda expression is a small, anonymous function that can be defined inline within a larger expression. It's a shorthand way to create a function without having to declare a separate named function.
+# syntax = lambda arguments: expression 
+
+add = lambda a,b : a+b
+print(add(5,6))
