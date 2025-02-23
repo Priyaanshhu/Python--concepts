45aea9e22c630731a0e7e0e761fc7bcbfa8134a7
Priyanshu Vishwakarma <144537220+Priyaanshhu@users.noreply.github.com>
2025-02-23 14:27:40 +0530

    Update enumerate.py

--- Advance build-in functions/enumerate.py
+++ Advance build-in functions/enumerate.py
@@ -1,3 +1,5 @@
+# enumerate() function 
+
 # enumerate is a built-in function that allows you to loop over a sequence (such as a list, tuple, or string) and have access to both the index and the value of each element.
 
 my_list = ['abc', 'xyz', 'mno']
